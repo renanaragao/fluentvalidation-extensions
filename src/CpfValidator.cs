1f1b08c0ab761c512ab4dc97d434d8d5a5df5cb8
Renan Aragão <refaragao@gmail.com>
2021-03-05 14:24:46 -0300

    adicionado validação de celular

---
+++ src/CpfValidator.cs
@@ -0,0 +1,30 @@
+﻿using FluentValidation.Validators;
+using Flunt.Br.Extensions;
+using Flunt.Validations;
+
+namespace FluentValidation.Extensions
+{
+    internal class CpfValidator : PropertyValidator
+    {
+
+        private readonly Contract contract;
+
+        public CpfValidator() : base("CPF_VALIDATOR_MESSAGE")
+        {
+            contract = new Contract();
+        }
+
+        protected override bool IsValid(PropertyValidatorContext context)
+        {
+            var value = context.PropertyValue as string;
+
+            if (value != null && !contract.IsCpf(value, string.Empty, string.Empty).Valid)
+            {
+                context.MessageFormatter.AppendArgument(nameof(CpfValidator), value);
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
