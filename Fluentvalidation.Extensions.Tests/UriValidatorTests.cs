2f9c2e87b6fedded3233eb0aff2290eb94d23b62
Renan Aragão <refaragao@gmail.com>
2021-03-05 12:29:09 -0300

    first

---
+++ Fluentvalidation.Extensions.Tests/UriValidatorTests.cs
@@ -0,0 +1,47 @@
+﻿using FluentValidation;
+using FluentValidation.Extensions;
+using Xunit;
+
+namespace Fluentvalidation.Extensions.Tests
+{
+    public class UriValidatorTests
+    {
+        private readonly AbstractValidator<UriTest> validator;
+
+        public UriValidatorTests()
+        {
+            validator = new UriValidatorTest();
+        }
+
+
+        [Theory]
+        [InlineData("https//testarrr")]
+        [InlineData("httptestarrr")]
+        [InlineData("https:/testarrr")]
+        [InlineData("https:testarrr")]
+        [InlineData("")]
+        [InlineData(" ")]
+        [InlineData(null)]
+        public void Must_Uri_Is_InValid(string uri)
+        {
+            var result = validator.Validate(new UriTest { Uri = uri });
+
+            Assert.False(result.IsValid);
+            Assert.Equal("'Uri' não é uma URI válida.", result.Errors[0].ErrorMessage);
+            Assert.Equal(nameof(UriValidator), result.Errors[0].ErrorCode);
+        }
+    }
+
+    class UriValidatorTest : AbstractValidator<UriTest>
+    {
+        public UriValidatorTest()
+        {
+            RuleFor(x => x.Uri).Uri();
+        }
+    }
+
+    class UriTest
+    {
+        public string Uri { get; set; }
+    }
+}
---
+++ src/IntegerValidator.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation.Validators;
+
+namespace FluentValidation.Extensions
+{
+    internal class IntegerValidator : PropertyValidator
+    {
+        public IntegerValidator()
+            : base("INTEGER_VALIDATOR_MESSAGE")
+        {
+        }
+
+        protected override bool IsValid(PropertyValidatorContext context)
+        {
+            var value = context.PropertyValue as string;
+
+            if (value != null && !int.TryParse(value, out var _))
+            {
+                context.MessageFormatter.AppendArgument(nameof(IntegerValidator), value);
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
